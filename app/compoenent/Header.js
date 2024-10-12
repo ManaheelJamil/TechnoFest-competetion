223575a640958651a4b48cb6161a1baa1db2016e
manaheel jamil <manaheeljamil20@gmail.com>
2024-10-12 19:44:39 +0500

    Add Header

---
+++ app/compoenent/Header.js
@@ -0,0 +1,10 @@
+const Header =()=>{
+return (
+    <>
+    <div className="bg-black text-white text-center text-3xl font-bold w-full p-5">
+        <h1>Navbar</h1>
+    </div>
+    </>
+)
+}
+export default Header
