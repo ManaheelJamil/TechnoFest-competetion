8735b5e517ca3ea789566797f0b0d6393eb816e4
manaheel jamil <manaheeljamil20@gmail.com>
2024-10-13 15:11:35 +0500

    techno fest

---
+++ app/About/page.js
@@ -0,0 +1,12 @@
+import Overview from "../component/Overview"
+import Speaker from "../component/Speaker"
+import Discover  from "../component/Discover"
+export default function About (){
+    return(
+<>
+<Overview/>
+<Speaker/>
+<Discover/>
+</>
+    )
+}
--- app/component/Navbar.js
+++ app/component/Navbar.js
@@ -19,9 +19,9 @@ export default function Navbar() {
                 <Image src="/logo.webp" alt="logo" width={100} height={100} />
                 <div className="lg:block hidden">
                     <ul className="flex items-center space-x-5">
-                        <li className="text-xl text-white font-bold "><Link href="">Home</Link></li>
-                        <li className="text-xl text-white font-bold "><Link href="">About us </Link></li>
-                        <li className="text-xl text-white font-bold "><Link href="">Competetion</Link></li>
+                        <li className="text-xl text-white font-bold "><Link href="/">Home</Link></li>
+                        <li className="text-xl text-white font-bold "><Link href="/About">About us </Link></li>
+                        <li className="text-xl text-white font-bold "><Link href="/Comptetion">Competetion</Link></li>
                         <li className="text-xl text-white font-bold "><Link href="">Medcon</Link></li>
                         <li className="text-xl text-white font-bold "><Link href="">Contact us</Link></li>
                     </ul>
@@ -42,14 +42,14 @@ export default function Navbar() {
             </div>
             {dropDown && (
                 <div>
-                    <div className="bg-blue-700 absolute left-0 right-0 z-40 py-10">
+                    <div className="gradient fixed top-28   left-0 right-0 z-40 py-10">
 
                         <ul className="text-center space-y-5 ">
-                            <li className="text-xl text-white font-bold "><Link href="">Home</Link></li>
-                            <li className="text-xl text-white font-bold "><Link href="">About us </Link></li>
-                            <li className="text-xl text-white font-bold "><Link href="">Competetion</Link></li>
-                            <li className="text-xl text-white font-bold "><Link href="">Medcon</Link></li>
-                            <li className="text-xl text-white font-bold "><Link href="">Contact us</Link></li>
+                            <li className="text-xl text-white font-bold " onClick={handleCLose}><Link href="/">Home</Link></li>
+                            <li className="text-xl text-white font-bold " onClick={handleCLose}><Link href="/About">About us </Link></li>
+                            <li className="text-xl text-white font-bold " onClick={handleCLose}><Link href="/Comptetion">Competetion</Link></li>
+                            <li className="text-xl text-white font-bold " onClick={handleCLose}><Link href="">Medcon</Link></li>
+                            <li className="text-xl text-white font-bold " onClick={handleCLose}><Link href="">Contact us</Link></li>
                         </ul>
                     </div>
                 </div>
